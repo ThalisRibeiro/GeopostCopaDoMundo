1110dde86d929a33547acc8aca4e71b82b599011
Thalis Ribeiro <thalis199@outlook.com>
2022-10-11 17:08:11 -0300

    Algoritimo que roda a partida e decide a quantidade de gols para cada time

---
+++ dist/js/partida.js
@@ -0,0 +1,25 @@
+"use strict";
+class Partida {
+    constructor() { }
+    RodaPartida(timeA, timeB) {
+        var gols = [2];
+        gols[0] = Math.floor(Math.random() * 4);
+        gols[1] = Math.floor(Math.random() * 4);
+        timeA.golsFeitos = gols[0];
+        timeA.golsRecebidos = gols[1];
+        timeB.golsFeitos = gols[1];
+        timeB.golsRecebidos = gols[0];
+        if (gols[0] > gols[1]) {
+            timeA.fimDeJogo(3, gols[0], gols[1]);
+            timeB.fimDeJogo(0, gols[1], gols[0]);
+        }
+        else if (gols[0] < gols[1]) {
+            timeA.fimDeJogo(0, gols[0], gols[1]);
+            timeB.fimDeJogo(3, gols[1], gols[0]);
+        }
+        else {
+            timeA.fimDeJogo(1, gols[0], gols[1]);
+            timeB.fimDeJogo(1, gols[1], gols[0]);
+        }
+    }
+}
