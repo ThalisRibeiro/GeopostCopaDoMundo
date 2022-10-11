c285412883d55f17eb316bb5949052ac61be471c
Thalis Ribeiro <thalis199@outlook.com>
2022-10-11 16:57:31 -0300

    buscador das informacoes da api

---
+++ dist/js/api.js
@@ -0,0 +1,36 @@
+"use strict";
+// function getInfoFetch(){
+//     const url = 'https://estagio.geopostenergy.com/WorldCup/GetAllTeams';
+//     var teste;
+//     fetch(url,{
+//         method: 'GET',
+//         headers:{
+//             'git-user': 'thalisribeiro'
+//         }
+//     })
+//     .then((response)=>{
+//         // console.log(response.json());
+//         return response.json();
+//     }).then(corpo=>{{
+//         console.log(corpo.Result);
+//         teste = corpo.Result;
+//         console.log(teste[1]);
+//     }})
+//     return teste
+// }
+async function fetchTeams() {
+    const url = 'https://estagio.geopostenergy.com/WorldCup/GetAllTeams';
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'git-user': 'thalisribeiro'
+            }
+        });
+        const data = await response.json();
+        return data.Result;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
