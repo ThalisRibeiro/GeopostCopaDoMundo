3ab2e9cbaac4821a15fe38c8f9c304989b8a6d65
Thalis Ribeiro <thalis199@outlook.com>
2022-10-12 15:17:42 -0300

    retorno do array de times que passaram de fase

--- dist/js/mataMata.js
+++ dist/js/mataMata.js
@@ -7,10 +7,22 @@ class MataMata {
         console.log('entramos nas partidas das oitavas');
         var partidas = new Partida();
         for (let index = 0; index < this._allTeams.length; index += 2) {
-            partidas.RodaPartida(this._allTeams[index], this._allTeams[index + 1]);
+            partidas.RodaPartidaMata(this._allTeams[index], this._allTeams[index + 1]);
             console.log(this._allTeams[index].nome + ' gols = ' + this._allTeams[index]._golsFeitosLastPartida + ' ' + this._allTeams[index + 1].nome + ' gols = ' + this._allTeams[index + 1]._golsFeitosLastPartida);
         }
         // this.mostraSaldoGols();
         // this.passaTimesDeFase();
+    }
+    proximaFase() {
+        console.log('Para proxima fase: ');
+        var ganharam = new Array;
+        for (let index = 0; index < this._allTeams.length; index++) {
+            //console.log(this._allTeams[index].nome+' passou de fase: '+ this._allTeams[index].passouDeFase);
+            if (this._allTeams[index].passouDeFase == true) {
+                ganharam.push(this._allTeams[index]);
+                console.log(this._allTeams[index]);
+            }
+        }
+        return ganharam;
     }
 }
--- src/mataMata.ts
+++ src/mataMata.ts
@@ -9,11 +9,26 @@ class MataMata {
         var partidas:Partida = new Partida();
         for (let index = 0; index < this._allTeams.length; index+=2) 
         {
-            partidas.RodaPartida(this._allTeams[index],this._allTeams[index+1]);   
+            partidas.RodaPartidaMata(this._allTeams[index],this._allTeams[index+1]);   
             console.log(this._allTeams[index].nome+' gols = '+this._allTeams[index]._golsFeitosLastPartida + ' '+this._allTeams[index+1].nome+' gols = '+this._allTeams[index+1]._golsFeitosLastPartida);       
         }
         
         // this.mostraSaldoGols();
         // this.passaTimesDeFase();
+    }
+    proximaFase():Array<Selecao>{
+        console.log('Para proxima fase: ');
+        
+        var ganharam: Array<Selecao> = new Array<Selecao>;
+        for (let index = 0; index < this._allTeams.length; index++) {
+            //console.log(this._allTeams[index].nome+' passou de fase: '+ this._allTeams[index].passouDeFase);
+            
+            if (this._allTeams[index].passouDeFase == true)
+            {
+                ganharam.push(this._allTeams[index]);   
+                console.log(this._allTeams[index]);
+            }
+        }
+        return ganharam;
     }
 }
