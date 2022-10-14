87dc7b0aae58c6e8354a826e93272e294feb163f
Thalis Ribeiro <thalis199@outlook.com>
2022-10-14 20:24:01 -0300

    Classe de dados das finais para serem mandados

---
+++ dist/js/dadosFinais.js
@@ -0,0 +1,11 @@
+"use strict";
+class dadosFinais {
+    constructor(equipea, equipeb, golsA, golsB, GolsPA, golsPB) {
+        this.equipeA = equipea;
+        this.equipeB = equipeb;
+        this.golsEquipeA = golsA;
+        this.golsEquipeB = golsB;
+        this.golsPenaltiEquipeA = GolsPA;
+        this.golsPenaltiEquipeB = golsPB;
+    }
+}
---
+++ src/dadosFinais.ts
@@ -0,0 +1,16 @@
+class dadosFinais {
+    constructor(equipea:string,equipeb:string,golsA:number,golsB:number,GolsPA:number,golsPB:number) {
+        this.equipeA=equipea;
+        this.equipeB=equipeb;
+        this.golsEquipeA = golsA;
+        this.golsEquipeB = golsB;
+        this.golsPenaltiEquipeA = GolsPA;
+        this.golsPenaltiEquipeB = golsPB;
+    }
+    public equipeA:string;
+    public equipeB:string;
+    public golsEquipeA:number;
+    public golsEquipeB:number;
+    public golsPenaltiEquipeA:number;
+    public golsPenaltiEquipeB:number;
+}
